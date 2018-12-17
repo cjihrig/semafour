c9959753dd03c4ac55ce3071722fdfa9cc171813
Florian <florian@tentwentyfour.lu>
2018-12-17 14:49:39 +0100

    Remove code from different feature branch

--- src/semafour.cc
+++ src/semafour.cc
@@ -14,13 +14,8 @@ using v8::Persistent;
 using v8::String;
 using v8::Uint32;
 using v8::Value;
-using v8::False;
-using v8::True;
 
 Persistent<Function> Semafour::constructor;
-
-#define TRYWAIT_OK     0
-#define TRYWAIT_LOCKED 1
 
 #define THROW(type, msg)                                                      \
   (isolate->ThrowException(Exception::type(String::NewFromUtf8(isolate, msg))))
